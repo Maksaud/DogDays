3e20ad9f380dc7e890275b3fde80d7fe45a663ab
Maksaud <49394528+Maksaud@users.noreply.github.com>
2019-11-23 19:05:13 +0000

    Initial commit

---
+++ DogDays.js
@@ -0,0 +1,17 @@
+const myAge = 21; // This is my age
+let earlyYears = 2; //This is the variable earlyyears initialised as 2
+
+earlyYears = earlyYears * 10.5;// This reasigns earlyYears as itsleft multiplied by 10.5.
+
+let laterYears = myAge - 2;//This is a variable that is myage -2.
+
+laterYears*=4;// This is the dogyears variable
+
+console.log(laterYears, earlyYears);
+
+const myAgeInDogYears = earlyYears + laterYears;// This is the variable myAgeInDogYears which is early years added to laterYears.
+
+let myName = "Maksaud";
+myName.toLowerCase();//This is the variable my name which holds 'Maksaud' tith the method that converts a string to lower case letters.
+
+console.log(`my name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
